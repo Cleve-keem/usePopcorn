ea6bb61ee3cb9bbea289f321f1494597205759c8
Cleve-keem <belloharyourmidey@gmail.com>
2024-12-27 01:40:32 -0800

    successfully compeleted the api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -11,52 +11,52 @@ import MovieList from "./Components/MovieList";
 import WatchedSummary from "./Components/WatchedSumarry";
 import WatchedMovieList from "./Components/WatchedMovieList";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    title: "inception",
-    year: "2010",
-    poster:
-      "https://th.bing.com/th/id/OIP.ULu3ytbiO_JdNVHqx4mqiQHaLH?rs=1&pid=ImgDetMain",
-  },
-  {
-    imdbID: "tt0133093",
-    title: "The Matix",
-    year: "1999",
-    poster:
-      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcR5DoFtShSmClflZ0RzBj9JBMweU5IUVBCeEbbLeV2XPlCnTKNi&psig=AOvVaw0yJsotMJs3u2tlmswuOIap&ust=1734605253632000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOCw7aySsYoDFQAAAAAdAAAAABAE",
-  },
-  {
-    imdbID: "tt6751668",
-    title: "Parasite",
-    year: "2019",
-    poster:
-      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcQBeM99UrE8KJqgLw2TnRYKPsE-b2s1vv_FNiYt6CJDC6ZlnqLv&psig=AOvVaw0hGBdycT1z7UTRXQHb-Sig&ust=1734605394273000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNDImuaSsYoDFQAAAAAdAAAAABAE",
-  },
-];
+// const tempMovieData = [
+//   {
+//     imdbID: "tt1375666",
+//     title: "inception",
+//     year: "2010",
+//     poster:
+//       "https://th.bing.com/th/id/OIP.ULu3ytbiO_JdNVHqx4mqiQHaLH?rs=1&pid=ImgDetMain",
+//   },
+//   {
+//     imdbID: "tt0133093",
+//     title: "The Matix",
+//     year: "1999",
+//     poster:
+//       "https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcR5DoFtShSmClflZ0RzBj9JBMweU5IUVBCeEbbLeV2XPlCnTKNi&psig=AOvVaw0yJsotMJs3u2tlmswuOIap&ust=1734605253632000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOCw7aySsYoDFQAAAAAdAAAAABAE",
+//   },
+//   {
+//     imdbID: "tt6751668",
+//     title: "Parasite",
+//     year: "2019",
+//     poster:
+//       "https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcQBeM99UrE8KJqgLw2TnRYKPsE-b2s1vv_FNiYt6CJDC6ZlnqLv&psig=AOvVaw0hGBdycT1z7UTRXQHb-Sig&ust=1734605394273000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNDImuaSsYoDFQAAAAAdAAAAABAE",
+//   },
+// ];
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    title: "inception",
-    year: "2010",
-    poster:
-      "https://th.bing.com/th/id/OIP.ULu3ytbiO_JdNVHqx4mqiQHaLH?rs=1&pid=ImgDetMain",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    title: "Back To The Future",
-    year: "1985",
-    poster:
-      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiAUI52_-niASQabl_PqdkJQ-moVziM6ahfiYkZW9NSnhcciNw",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+// const tempWatchedData = [
+//   {
+//     imdbID: "tt1375666",
+//     title: "inception",
+//     year: "2010",
+//     poster:
+//       "https://th.bing.com/th/id/OIP.ULu3ytbiO_JdNVHqx4mqiQHaLH?rs=1&pid=ImgDetMain",
+//     runtime: 148,
+//     imdbRating: 8.8,
+//     userRating: 10,
+//   },
+//   {
+//     imdbID: "tt0088763",
+//     title: "Back To The Future",
+//     year: "1985",
+//     poster:
+//       "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQiAUI52_-niASQabl_PqdkJQ-moVziM6ahfiYkZW9NSnhcciNw",
+//     runtime: 116,
+//     imdbRating: 8.5,
+//     userRating: 9,
+//   },
+// ];
 
 function average(arr) {
   return arr.reduce((acc, cur) => acc + cur / arr.length, 0);
@@ -65,21 +65,35 @@ function average(arr) {
 const KEY = "7023817c";
 
 function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const query = "interstellar";
 
   useEffect(function () {
     async function fetchMovies() {
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-      );
-      const data = await res.json();
-      console.log(data);
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+        );
+
+        const data = await res.json();
+        setMovies(data.Search);
+        console.log(data.Search);
+      } catch (error) {
+        console.log(error.message)
+      }
+      finally{
+        setIsLoading(false);
+      }
     }
     fetchMovies();
   }, []);
+
+  function Loading() {
+    return <div className="loading" style={{textAlign: "center"}}>Loading...</div>;
+  }
 
   return (
     <div className="app">
@@ -101,9 +115,7 @@ function App() {
           }
         /> */}
 
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loading /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} average={average} />
--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -2,13 +2,13 @@ export default function Movie({ movie }) {
   return (
     <li className="movie">
       <div className="poster">
-        <img src={movie.poster} alt="Movie Poster" />
+        <img src={movie.Poster} alt="Movie Poster" />
       </div>
       <div className="desc">
-        <h4 className="name">{movie.title}</h4>
+        <h4 className="name">{movie.Title}</h4>
         <p>
           <span>📅</span>
-          <span>{movie.year}</span>
+          <span>{movie.Year}</span>
         </p>
       </div>
     </li>
